68770eea1e828877ed0ef222c25cd94ed8efc7fb
Adam Hoenich <adam.hoenich@acquia.com>
2018-08-28 13:01:50 -0400

    Simplify lightning_media_video 2.4.0 update

--- modules/lightning_media_video/src/Update/Update240.php
+++ modules/lightning_media_video/src/Update/Update240.php
@@ -2,83 +2,35 @@
 
 namespace Drupal\lightning_media_video\Update;
 
-use Drupal\Core\Config\ConfigFactoryInterface;
-use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-
 use Drupal\lightning_core\ConfigHelper;
 
 /**
  * @Update("2.4.0")
  */
-final class Update240 implements ContainerInjectionInterface {
+final class Update240 {
 
   /**
-   * The config factory.
-   *
-   * @var \Drupal\Core\Config\ConfigFactoryInterface
-   */
-  private $configFactory;
-
-  /**
-   * Update240 constructor.
-   *
-   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
-   *   The config factory.
-   */
-  public function __construct(ConfigFactoryInterface $config_factory) {
-    $this->configFactory = $config_factory;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container) {
-    return new static(
-      $container->get('config.factory')
-    );
-  }
-
-  /**
-   * Installs Lightning's Video file media type.
+   * Installs a media type for locally hosted video files.
    *
    * @update
    *
-   * @ask Do you want to install "Video file" media type?
+   * @ask Do you want to install the "Video file" media type?
    */
   public function installVideoFileMedia() {
-    $video_config_helper = ConfigHelper::forModule('lightning_media_video');
-    // Configs which could be imported simply.
-    $configs = [
-      ['media_type', 'video_file'],
-      ['field_storage_config', 'media.field_media_video_file'],
-      ['field_config', 'media.video_file.field_media_in_library'],
-      ['field_config', 'media.video_file.field_media_video_file'],
-      ['entity_form_display', 'media.video_file.media_browser'],
-      ['entity_view_display', 'media.video_file.embedded'],
-      ['entity_view_display', 'media.video_file.thumbnail'],
-    ];
-    // Default entity displays should be handled separately since these are
-    // created automatically during media type import.
-    $default_display_configs = [
-      'core.entity_form_display.media.video_file.default',
-      'core.entity_view_display.media.video_file.default',
-    ];
+    $helper = ConfigHelper::forModule('lightning_media_video');
 
-    // Importing simple configs.
-    foreach ($configs as $config_params) {
-      list($entity_type, $id) = $config_params;
-      $video_config_helper->getEntity($entity_type, $id)->save();
-    }
+    $helper->getEntity('media_type', 'video_file')->save();
+    $helper->getEntity('field_storage_config', 'media.field_media_video_file')->save();
+    $helper->getEntity('field_config', 'media.video_file.field_media_video_file')->save();
+    $helper->getEntity('field_config', 'media.video_file.field_media_in_library')->save();
+    $helper->getEntity('entity_form_display', 'media.video_file.media_browser')->save();
+    $helper->getEntity('entity_view_display', 'media.video_file.embedded')->save();
+    $helper->getEntity('entity_view_display', 'media.video_file.thumbnail')->save();
 
-    // Dealing with default displays.
-    foreach ($default_display_configs as $config_id) {
-      $new_display_data = $video_config_helper->read($config_id);
-
-      $this->configFactory->getEditable($config_id)
-        ->setData($new_display_data)
-        ->save(TRUE);
-    }
+    // Handle entity displays as simple config, since they are created
+    // automatically when the media type is imported.
+    $helper->get('core.entity_form_display.media.video_file.default')->save(TRUE);
+    $helper->get('core.entity_view_display.media.video_file.default')->save(TRUE);
   }
 
 }
