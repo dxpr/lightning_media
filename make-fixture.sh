8d9674877833712cae208dd8c14cab28c3ccc6d1
Phéna Proxima <adam@phenaproxima.net>
2019-11-13 18:04:51 -0500

    [skip ci] Add automated script to create a release branch, and use semantic version number for change logs.

--- make-fixture.sh
+++ make-fixture.sh
@@ -8,13 +8,15 @@ if [[ $BRANCH != release/* ]]; then
 fi
 
 VERSION=${BRANCH#release/}
+# Ask git for the most recent semantic version tag, and use it as the version
+# from which to update.
+FROM=$(git tag --list --sort -creatordate | head -n 2 | grep -E '^(\d+.?){3}$')
 
 ./install-drupal.sh
 
-# Destroy the database.
+# Destroy the database and import the fixture from which to update.
+echo "Replacing database with $FROM snapshot..."
 drush sql:drop --yes
-
-# Import the fixture from which to update.
 cd docroot
 php core/scripts/db-tools.php import ../tests/fixtures/$FROM.php.gz
 cd ..
--- release.sh
+++ release.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
 # Tags and pushes a release to origin and drupal.org.
-# Example usage: ./release.sh 2.2.0 8.x-2.2
+# Example usage: ./release.sh 2.2.0
 
-SEMANTIC_VERSION=$1
-DRUPAL_VERSION=$2
+DRUPAL_VERSION=8.x-${1%.0}
 
 PROJECT=${PWD##*/}
-RELEASE_BRANCH=release/$SEMANTIC_VERSION
+RELEASE_BRANCH=release/$1
 
 MARKDOWN=`command -v markdown`
+
+# Since this script does things that are pretty permanent, give the user 5
+# seconds to cancel without consequences.
+echo "Tagging $DRUPAL_VERSION from $RELEASE_BRANCH in 5 seconds..."
+sleep 5
 
 # Ensure we are on a mainline release branch.
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
@@ -18,7 +22,7 @@ if [[ $BRANCH =~ ^8\.x\-[0-9]+\.x$ ]]; then
   git remote add drupal-org git@git.drupal.org:project/$PROJECT.git
 
   git merge --squash $RELEASE_BRANCH
-  git commit --all --quiet --message "$SEMANTIC_VERSION Release"
+  git commit --all --quiet --message "$1 Release"
   git push --quiet origin
   git push --quiet drupal-org
   echo "Merged $RELEASE_BRANCH and pushed to origin and drupal.org."
@@ -26,9 +30,9 @@ if [[ $BRANCH =~ ^8\.x\-[0-9]+\.x$ ]]; then
   git branch -D $RELEASE_BRANCH
   git push origin :$RELEASE_BRANCH
 
-  git tag $SEMANTIC_VERSION
-  git push --quiet origin $SEMANTIC_VERSION
-  echo "Tagged $SEMANTIC_VERSION and pushed to origin."
+  git tag $1
+  git push --quiet origin $1
+  echo "Tagged $1 and pushed to origin."
 
   git tag $DRUPAL_VERSION
   git push --quiet drupal-org $DRUPAL_VERSION
@@ -37,10 +41,10 @@ if [[ $BRANCH =~ ^8\.x\-[0-9]+\.x$ ]]; then
   git remote remove drupal-org
 
   if [[ $MARKDOWN ]]; then
-    markdown logs/$DRUPAL_VERSION.md | pbcopy
+    markdown logs/$1.md | pbcopy
     echo "The change log has been copied to the clipboard as HTML."
   else
-    pbcopy < logs/$DRUPAL_VERSION.md
+    pbcopy < logs/$1.md
     echo "The change log has been copied to the clipboard as Markdown. You will need to manually convert it to HTML when creating the release."
     echo "If you want to skip this step next time, install markdown-to-html from https://github.com/cwjohan/markdown-to-html."
   fi
