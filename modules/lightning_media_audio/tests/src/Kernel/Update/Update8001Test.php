f6e66690dae69271c0ff1c694c5138d1ca3fc710
kdruckman <kdruckman@gmail.com>
2020-06-19 12:44:32 -0400

    Make bundled media types compatible with the Standard profile.

---
+++ modules/lightning_media_audio/tests/src/Kernel/Update/Update8001Test.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Drupal\Tests\lightning_media_audio\Kernel\Update;
+
+use Drupal\Core\Entity\Entity\EntityFormMode;
+use Drupal\KernelTests\KernelTestBase;
+use Drupal\Tests\media\Traits\MediaTypeCreationTrait;
+
+/**
+ * Tests Lightning Media Audio's update path.
+ *
+ * @group lightning_media_audio
+ * @group lightning_media
+ *
+ * @covers lightning_media_audio_update_8001()
+ */
+class Update8001Test extends KernelTestBase {
+
+  use MediaTypeCreationTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'field',
+    'lightning_media_audio',
+    'media',
+    'media_test_source',
+  ];
+
+  /**
+   * Tests the update function.
+   */
+  public function testUpdate() {
+    $this->createMediaType('test', ['id' => 'audio_file']);
+
+    EntityFormMode::create([
+      'targetEntityType' => 'media',
+      'id' => 'media.media_library',
+    ])->save();
+
+    module_load_install('lightning_media_audio');
+    lightning_media_audio_update_8001();
+  }
+
+}
---
+++ modules/lightning_media_video/tests/src/Kernel/Update80038004Test.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Drupal\Tests\lightning_media_video\Kernel;
+
+use Drupal\Core\Entity\Entity\EntityFormMode;
+use Drupal\Core\Entity\Entity\EntityViewMode;
+use Drupal\KernelTests\KernelTestBase;
+use Drupal\Tests\media\Traits\MediaTypeCreationTrait;
+
+/**
+ * Tests Lightning Media Video's update path.
+ *
+ * @group lightning_media_video
+ * @group lightning_media
+ *
+ * @covers lightning_media_video_update_8003()
+ * @covers lightning_media_video_update_8004()
+ */
+class Update80038004Test extends KernelTestBase {
+
+  use MediaTypeCreationTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'field',
+    'image',
+    'lightning_media_video',
+    'media',
+    'media_test_source',
+  ];
+
+  /**
+   * Tests the update function.
+   */
+  public function testUpdate() {
+    $this->createMediaType('test', ['id' => 'video']);
+
+    EntityViewMode::create([
+      'targetEntityType' => 'media',
+      'id' => 'media.thumbnail',
+    ])->save();
+
+    EntityFormMode::create([
+      'targetEntityType' => 'media',
+      'id' => 'media.media_library',
+    ])->save();
+
+    module_load_install('lightning_media_video');
+    lightning_media_video_update_8003();
+    lightning_media_video_update_8004();
+  }
+
+}
